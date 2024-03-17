e73b7b2d472a16aedba124416cd059484700ffca
Trebain <154022424+Trebein@users.noreply.github.com>
2024-03-18 01:20:10 +0600

    Update bluetoothctl.sh

--- bluetoothctl.sh
+++ bluetoothctl.sh
@@ -4,6 +4,10 @@ default-agent
 power on
 scan on
 
+# Keychron K3 DC:2C:26:42:03:B2
+pair E8:EE:CC:AD:97:DA
+connect E8:EE:CC:AD:97:DA
+
 # Soundcore Space Q45 E8:EE:CC:AD:97:DA
 pair E8:EE:CC:AD:97:DA
 connect E8:EE:CC:AD:97:DA
@@ -12,6 +16,4 @@ connect E8:EE:CC:AD:97:DA
 pair E8:EE:CC:22:EC:B4
 connect E8:EE:CC:22:EC:B4
 
-# Keychron K3 DC:2C:26:42:03:B2
-pair E8:EE:CC:AD:97:DA
-connect E8:EE:CC:AD:97:DA
+echo Устройства подключены
