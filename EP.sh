845e03f8d3f51185cddc5807e136213307672c30
Trebain <154022424+Trebein@users.noreply.github.com>
2024-04-13 19:32:38 +0500

    Update EP.sh

--- EP.sh
+++ EP.sh
@@ -39,6 +39,7 @@ gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffe
    flatpak install flathub org.telegram.desktop --noconfirm  # telegram
    flatpak install flathub io.atom.Atom #
    flatpak install flathub io.crow_translate.CrowTranslate
+   flatpak install flathub fr.nytuo.cosmiccomics
 
 # Snap пакеты
    # git clone https://aur.archlinux.org/snapd.git
