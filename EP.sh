c383b9b9bba410e3c3fb9d3f1f6a83764c100514
Trebain <154022424+Trebein@users.noreply.github.com>
2024-04-16 16:55:57 +0500

    Update EP.sh

--- EP.sh
+++ EP.sh
@@ -70,12 +70,13 @@ sudo pacman -R gnome-tour
    # команда для установки расширений в терминале наверняка должна существовать
 
 # Steam
-  sudo pacman -S steam-native-runtime
-  sudo pacman -S wine
+  # пожалуй не сильно хочется качать говно из аура скриптом
+  # sudo pacman -S steam-native-runtime
+  # sudo pacman -S wine
   # протон ге
-  yay -S protontricks
-  yay -S winetricks
-  yay -S python-vdf
+  # yay -S protontricks
+  # yay -S winetricks
+  # yay -S python-vdf
   sudo pacman -S yad # protontricks --gui
 
 # обновить все пакеты, вывести и удалить осиротевшие пакеты
